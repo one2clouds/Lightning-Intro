b8c12020100da9ed63d86a9996d0208caecf070c
Shirshak <shirshak.acharya@gmail.com>
2024-04-13 21:07:22 +0545

    -

--- train.py
+++ train.py
@@ -1,4 +1,9 @@
 import torch
+import config
+from callbacks import MyPrintingCallback, EarlyStopping
+from model import NN
+from dataset import MnistDataModule
+import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.strategies import DeepSpeedStrategy
 from pytorch_lightning.profilers import PyTorchProfiler
@@ -12,6 +17,28 @@ if __name__ == "__main__":
     profiler = PyTorchProfiler(
         on_trace_ready=torch.profiler.tensorboard_trace_handler("tb_logs/profiler0"),
         schedule=torch.profiler.schedule(skip_first=10, wait=1, warmup=1, active=20),
-    
-
     )
+    model = NN(
+        input_size=config.INPUT_SIZE,
+        learning_rate=config.LEARNING_RATE,
+        num_classes=config.NUM_CLASSES,
+    )
+    dm = MnistDataModule(
+        data_dir=config.DATA_DIR,
+        batch_size=config.BATCH_SIZE,
+        num_workers=config.NUM_WORKERS,
+    )
+    trainer = pl.Trainer(
+        strategy=strategy,
+        profiler=profiler,
+        logger=logger,
+        accelerator=config.ACCELERATOR,
+        devices=config.DEVICES,
+        min_epochs=1,
+        max_epochs=config.NUM_EPOCHS,
+        precision=config.PRECISION,
+        callbacks=[MyPrintingCallback(), EarlyStopping(monitor="val_loss")],
+    )
+    trainer.fit(model, dim)
+    trainer.validate(model,dm)
+    trainer.test(model,dm)
